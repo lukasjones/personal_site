d395c654561b9f37dd827e6f8f89ce8b52e8e806
Lukas Jones <joneslukas21@gmail.com>
2015-06-08 12:45:37 -0700

    got navbar to display titles. routes and display blog post are not working yet

--- app/assets/javascripts/components/Blog.js.jsx
+++ app/assets/javascripts/components/Blog.js.jsx
@@ -5,7 +5,7 @@ var Blog = React.createClass({
 			type: "get",
 			dataType: 'json',
 			success: function(blogs) {
-				this.setState({blogs: blogs});
+				this.setState({blogs: blogs, currentBlogId: blogs.length-1});
 			}.bind(this),
 			error: function(data){
 				console.log("didn't work");
@@ -13,25 +13,25 @@ var Blog = React.createClass({
 		});
 	},
 	getInitialState: function(){
-		return {blogs: []}
+		return {blogs: [], currentBlogId: null}
 	},
 	componentDidMount: function(){
 		this.loadBlogs();
 	},
 	render: function(){
 		var blogs = this.state.blogs;
-		console.log(blogs);
+		
 		return (
 			<div className="wrap">
 				<div id="layout">
 					<div id="main">
-							<NavBar />
+							<NavBar blogs={this.state.blogs} currentBlogId={this.state.currentBlogId}/>
 					</div>
 				</div>
 
 				<div id="layout">
 					<div id="main">
-							<BlogPost blogs={this.state.blogs}/>
+							<BlogPost blogs={this.state.blogs} currentBlogId={this.state.currentBlogId} />
 					</div>
 				</div>
 			</div>
@@ -41,6 +41,8 @@ var Blog = React.createClass({
 
 var NavBar = React.createClass({
 	render: function(){
+		console.log(this.props.currentBlogId)
+		var currentBlogId = this.props.currentBlogId
 		return (
 			<div>
 				<a href="#menu" id="menuLink" className="menu-link">
@@ -52,14 +54,21 @@ var NavBar = React.createClass({
 		        <a className="pure-menu-heading" href="/">Home</a>
 
 		        <ul className="pure-menu-list">
-		            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog 1</a></li>
-		            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog 2</a></li>
-
-		            <li className="pure-menu-item" className="menu-item-divided pure-menu-selected">
-		                <a href="#" className="pure-menu-link">Blog 3</a>
-		            </li>
-
-		            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog 4</a></li>
+		        		{
+		        			this.props.blogs.map(function(blog){
+			        			if (currentBlogId !== null) {
+			        				if ((blog.id - 1) === currentBlogId) {
+			        					return (
+			        						<SelectedNavItem href={blog.id}>{blog.title}</SelectedNavItem>
+			        					)
+			        				} else {
+				        				return (
+				        					<NavItem href={blog.id} >{blog.title}</NavItem>
+				        				)
+			        				}
+				        		} 
+		        			})
+		        		}
 		        </ul>
 			    </div>
 				</div>
@@ -68,28 +77,50 @@ var NavBar = React.createClass({
 	}
 });
 
-var BlogPost = React.createClass({
+var NavItem = React.createClass({
 	render: function(){
 		return (
-			// <h1 className="blog-title">{this.props.title}</h1>
-			// <span className="blog-date">{this.props.date}</span>
-			// <p className="blog-content">{this.props.content}</p>
-			<div id="main">
-			  <div className="header">
-	        <h1>{this.props.title}</h1>
-	        <h2>{this.props.date}</h2>
-		    </div>
-
-		    <div className="content">
-	        <h2 className="content-subhead">Post Written By Lukas Jones</h2>
-	        <p>
-	        	{this.props.content}
-	        </p>
-
-		    </div>
-			</div>
-		);
+			<li className="pure-menu-item"><a href={this.props.href} className="pure-menu-link">{this.props.children}</a></li>
+		)
 	}
+})
+
+var SelectedNavItem = React.createClass({
+	render: function(){
+		return (
+			<li className="pure-menu-item" className="menu-item-divided pure-menu-selected">
+			    <a href={this.props.href} className="pure-menu-link">{this.props.children}</a>
+			</li>
+		)
+	}
+})
+
+
+
+var BlogPost = React.createClass({
+	render: function(){
+		var currentBlogId = this.props.currentBlogId;
+		var currentBlog = this.props.blogs[currentBlogId]
+			return (
+				// <h1 className="blog-title">{this.props.title}</h1>
+				// <span className="blog-date">{this.props.date}</span>
+				// <p className="blog-content">{this.props.content}</p>
+				<div id="main">
+				  <div className="header">
+		        <h1>{" " || currentBlog.title}</h1>
+		        <h2>{" " || currentBlog.created_at}</h2>
+			    </div>
+
+			    <div className="content">
+		        <h2 className="content-subhead">Post Written By Lukas Jones</h2>
+		        <p>
+		        	{" " || currentBlog.content}
+		        </p>
+
+			    </div>
+				</div>
+			);
+		}
 });
 
 
