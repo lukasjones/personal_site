fcda1e44ea98f6c07bcf7653d72acf2416c965dc
Lukas Jones <joneslukas21@gmail.com>
2015-06-15 22:33:21 -0700

    added skills

---
+++ app/assets/javascripts/bio.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+
+
+	$(".skill-bar").each(function(i, blah){
+		var width = $(this).find(".inner-bar").attr("data-skill") + "%";
+		$(this).find(".inner-bar").css("width", width);
+	})
+
+
+	$(".skill-bar").on("click", function(){
+		$(this).find(".description").slideToggle();
+	})
+
+
+
+})
